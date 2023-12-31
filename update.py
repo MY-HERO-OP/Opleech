e12329e1352cb54d025957934035f358a0f712be
MY-HERO-OP <129320317+MY-HERO-OP@users.noreply.github.com>
2024-01-01 02:23:01 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,39 +1,32 @@
-from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, execl as osexecl
-from subprocess import run as srun
-from requests import get as rget
-from dotenv import load_dotenv
-from sys import executable
+import sys
+
+from base64 import b64decode
+from dotenv import load_dotenv, dotenv_values
+from logging import FileHandler, StreamHandler, basicConfig, error as log_error, info as log_info, INFO
+from os import path as ospath, environ, remove
 from pymongo import MongoClient
+from re import sub as resub
+from requests import get as rget
+from subprocess import run as srun
+
 
 if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
 
-basicConfig(format='%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s',
-                    handlers=[FileHandler('log.txt'), StreamHandler()], level=INFO)
+if ospath.exists('rlog.txt'):
+    remove('rlog.txt')
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
+basicConfig(format='%(asctime)s: [%(levelname)s: %(filename)s - %(lineno)d] ~ %(message)s',
+            handlers=[FileHandler('log.txt'), StreamHandler()],
+            datefmt='%d-%b-%y %I:%M:%S %p',
+            level=INFO)
 
 load_dotenv('config.env', override=True)
 
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 if len(BOT_TOKEN) == 0:
-    log_error("BOT_TOKEN variable is missing! Exiting now")
+    error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = BOT_TOKEN.split(':', 1)[0]
@@ -56,12 +49,12 @@ if len(UPSTREAM_REPO) == 0:
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'test'
+    UPSTREAM_BRANCH = 'main'
 
-if ospath.exists('.git'):
-    srun(["rm", "-rf", ".git"])
+if path.exists('.git'):
+    run(["rm", "-rf", ".git"])
 
-update = srun([f"git init -q \
+update = run([f"git init -q \
                  && git config --global user.email yesiamshojib@gmail.com \
                  && git config --global user.name 5hojib \
                  && git add . \
@@ -71,6 +64,6 @@ update = srun([f"git init -q \
                  && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
 if update.returncode == 0:
-    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    info('Successfully updated with latest commit from UPSTREAM_REPO')
 else:
-    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+    error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
